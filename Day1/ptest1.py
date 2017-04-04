347242200546e44255aed498f3f4f601963d2081
root <root@localhost.localdomain>
2017-04-04 12:52:35 -0400

    This is commit to see if the code works

--- Day1/ptest1.py
+++ Day1/ptest1.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
+from __future__ import division
 
 def primeAnalysis(b):
     y = ''
-    for x in range(0, b):
-        if x > 1:
-            if ( x % 2 != 0):
-                y += str(x)
+
+    if b > 1:
+        for x in range(0, b):
+            if ( b % x ) == 0:
+                break
+            else:
+                y += str(x) + ','
 
         x = int(x)
         x += 1
 
+    return y
 
-b= int(raw_input("How many numbers you wish to check: "))
+
+b= int(input("How many numbers you wish to check: "))
 
 response = primeAnalysis(b)
 
-print response
+print (response)
