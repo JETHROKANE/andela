23bd7ef819d3b54189892d08f3a60162c182bac1
root <root@localhost.localdomain>
2017-04-04 13:58:15 -0400

    Atlast it works

--- Day1/ptest1.py
+++ Day1/ptest1.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python
-from __future__ import division
+#from __future__ import division
 
 def primeAnalysis(b):
     y = ''
 
-    if b > 1:
-        for x in range(0, b):
-            if ( b % x ) == 0:
-                break
+    for x in range(0, b + 1):
+        if x > 1:
+            for n in range(2, x):
+                if (x % n) == 0:
+                    break
             else:
                 y += str(x) + ','
 
-        x = int(x)
-        x += 1
 
     return y
 
 
-b= int(input("How many numbers you wish to check: "))
+b = int(input("How many numbers you wish to check: "))
 
 response = primeAnalysis(b)
 
