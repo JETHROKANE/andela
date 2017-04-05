a07f38549e88ee6458b567ecaf3b8cfdc3acb851
JETHROKANE <vinyltroso@outlook.com>
2017-04-05 15:58:34 +0300

    This are day 3 scripts for the weather wsdl script

--- Day3/ptest3.py
+++ Day3/ptest3.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python
 
-import unittest
-from ptest2 import find_max_min
+from suds.client import Client
 
-class MaxMinTest(unittest.TestCase):
-    """docstring for MaxMinTest"""
+def Long_lat(a):
 
-    def test_find_max_min_four(self):
-        self.assertListEqual([1, 4],
-                             find_max_min([1, 2, 3, 4]),
-                             msg='should return [1,4] for [1, 2, 3, 4]')
+    url = 'http://www.webservicex.com/airport.asmx?wsdl'
+    client = Client(url, timeout=600)
+    results = client.service.getAirportInformationByCityOrAirportName(a)
 
-    def test_find_max_min_one(self):
-        self.assertListEqual([4, 6],
-                             find_max_min([6, 4]),
-                             msg='should return [4, 6] for [6, 4]')
+    return results
 
-    def test_find_max_min_two(self):
-        self.assertListEqual([2, 78],
-                             find_max_min([4, 66, 6, 44, 7, 78, 8, 68, 2]),
-                             msg='should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]')
 
-    def test_find_max_min_three(self):
-        self.assertListEqual([1, 4],
-                             find_max_min([1, 2, 3, 4]),
-                             msg='should return [1,4] for [1, 2, 3, 4]')
+a = "paris"
 
-    def test_find_max_min_identity(self):
-        self.assertListEqual([4],
-                             find_max_min([4, 4, 4, 4]),
-                             msg='Return the number of elements in the list in a new list if the `min` and `max` are equal')
-
-if __name__ == '__main__':
-    unittest.main()
+results = Long_lat(a)
+print results
