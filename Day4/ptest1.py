1d2d522be4ac154e863df9c1b680f442d9cbb315
JETHROKANE <vinyltroso@outlook.com>
2017-04-06 11:53:10 +0300

    This adds day 4 first script

--- Day4/ptest1.py
+++ Day4/ptest1.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 
 def find_missing(a, b):
-    y = []
-    for x in a:
+    z = []
+    m = len(a)
+    n = len(b)
+    if m == n:
+        k = 0
+        return k
+    else:
+        for x in b:
+            y = a.count(x)
+
+            if y == 0:
+                return x
+
+
+
 
